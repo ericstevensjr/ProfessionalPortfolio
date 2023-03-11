79c5174aff26e843144cfa975aa4910602913c22
Eric Stevens Jr <ericstevensjrprofessional@outlook.com>
2023-03-10 21:19:01 -0500

    First prototype of website

---
+++ script.js
@@ -0,0 +1,28 @@
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    nav.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    nav.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+const headerImages = document.querySelector('.header-images');
+
+// Add event listener to window scroll event
+window.addEventListener('scroll', () => {
+  if (window.scrollY > header.offsetHeight) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
+``
+
